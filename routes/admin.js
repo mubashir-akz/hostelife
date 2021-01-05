31abed5747b8678ed8f66f47c8be24f4335f35e7
Mubashir-akz <mubashir.jr520@gmail.com>
2021-01-05 15:44:23 +0530

    admin template added

--- routes/admin.js
+++ routes/admin.js
@@ -2,8 +2,11 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-    res.render('admin/index')
-})
+    res.render('admin/index', {
+        admin: true
+    });
+});
+
 
 
 module.exports = router;
