b83dd37eb6f7849aa08f73d1e1eb1985ea58e069
Mubashir-akz <mubashir.jr520@gmail.com>
2021-01-06 21:45:51 +0530

    hostel owner login added

--- routes/hostel-owner.js
+++ routes/hostel-owner.js
@@ -2,13 +2,14 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/home', function(req, res, next) {
   res.render('hostelOwner/home',{hostelowner:true});
 });
-router.get('/signup',(req,res)=>{
-  res.render('Hostel/signup')
+router.get('',(req,res)=>{
+  res.render('hostelOwner/login')
 })
 router.post('/register',(req,res)=>{
   console.log(req.body);
 })
+
 module.exports = router;
