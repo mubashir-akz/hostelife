3b0cbd0170a7e3fdf305857faea0929becd4dd3a
Mubashir-akz <mubashir.jr520@gmail.com>
2021-01-14 00:21:01 +0530

    adding momentjs

--- d.js
+++ d.js
@@ -1,17 +1,1 @@
-const datas = [
-    {
-        name: 1,
-        age: 2,
-    }, 
-    {
-        name: 3,
-        age: 3
-    }
-]
-
-var a = datas.map((val)=>{
-    // console.log(val);
-    return val.age = 500
-})
-
-console.log(a);
+console.log(Date());
